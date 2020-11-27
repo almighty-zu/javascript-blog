1362fc9ae300eb4658dc0447805731d637e1c490
Zuzanna Szeloch <zuz.szeloch@gmail.com>
2020-11-27 17:53:36 +0100

    Add const generateTags with planed algorithm

--- js/script.js
+++ js/script.js
@@ -56,9 +56,10 @@ const titleClickHandler = function (event) {
   const optArticleSelector = '.post',
     optTitleSelector = '.post-title',
     optTitleListSelector = '.titles';
+    optArticleTagsSelector = '.post-tags .list';
 
   const GenerateTitleLinks = function () {
-    console.log('Title links were generated!');
+    console.log('Title links are generated!');
 
 
     /* [DONE] remove contents of titleList*/
@@ -109,4 +110,34 @@ const titleClickHandler = function (event) {
 
   GenerateTitleLinks();
 
+  conts generateTags = function (){
+
+    /* find all articles */
+
+    /* START LOOP: for every article: */
+
+    /* find tags wrapper */
+
+    /* make html variable with empty string */
+
+    /* get tags from data-tags attribute */
+
+    /* split tags into array */
+
+    /* START LOOP: for each tag */
+
+    /* generate HTML of the link */
+
+    /* add generated code to html variable */
+
+    /* END LOOP: for each tag */
+
+    /* insert HTML of all the links into the tags wrapper */
+
+    /* END LOOP: for every article: */
+  };
+
+  generateTags();
+
 }
+
